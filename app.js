47eb4ff5c9864b23d8ead05a25047cf8e7f7cf90
alvaro valencia <alvaroleandro74@gmail.com>
2022-05-24 13:38:56 -0300

    trabajando con los datos ingresados por el usuario

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 document.write('clase n°: 4 / fecha: 5/5 / alumno: Alvaro Valencia');
 // Carrito de Compras
 /*Declaracion de variables */
@@ -22,7 +21,7 @@ let listaCompras = [];
 ************************************AGREGANDO CLASES DE OBJETOS AL CARRITO*********************************************
 ******************************************************************************************************************** */
 
-class indumentariaVendida{
+class indumentaria{
     constructor(tipo,talle,precio,color,material){
         this.tipo = tipo;
         this.talle = talle;
@@ -35,22 +34,6 @@ class indumentariaVendida{
         this.precio = precio * 1.21;
     }
 }
-
-/* class indumentariaRegistrada{
-    constructor(tipo,talle,precio,color,material,codigo){
-        this.tipo = tipo;
-        this.talle = talle;
-        this.precio = precio;
-        this.color = color;
-        this.material = material;
-        this.codigo = codigo;
-        this.vendido = false;
-    }
-    iva() {
-        this.precio = precio * 1.21;
-    }
-} */
-
 function seleccionCaracteristicas (producto){
     switch ( parseFloat(producto)) {
         case 1: //Pantalon
@@ -277,34 +260,218 @@ let memoria = (a) =>{
 
 /* MAIN */
 
-alert('| Bienvenido a tienda AIVA |  \n A continuacion ingrese el codigo de los articulos que desee comprar. para finalizar ingrese (N) \n Gracias! ' )
+/* alert('| Bienvenido a tienda AIVA |  \n A continuacion ingrese el codigo de los articulos que desee comprar. para finalizar ingrese (N) \n Gracias! ' )
 
-do {
-    
+do {    
     door = prompt  ('Desea continuar operando? Ingrese (Y) para continuar ó (N) para finalizar el proceso');
     if( door == 'y' || door == 'Y'  ) {
         producto = prompt('- TIENDA DE ROPA - \n | Articulos en venta |  \n Para seleccionar un producto ingrese el numero del mismo. \n -Pantalon | Numero de producto (1)\n -Remera | Numero de producto (2)\n -Buzo | Numero de producto (3)\n -Gorras | Numero de producto (4)\n -Accesorio| Numero de producto (5)\n)' )
         seleccionCaracteristicas(producto); 
     } 
-
 }while (door == 'y' || door == 'Y');
 
 console.log(listaCompras);
-alert('Proceso Finalizado.\n Fecha de la compra: '+ fecha.toLocaleString()+' \n El total de tu compra es:  '+ Number(totalCompras) +' \n La cantidad de productos comprados es de : '+ Number(listaCompras.length) +'\n | VER EN CONSOLA SUS COMPRAS Y CARACTERISTICAS |');
+alert('Proceso Finalizado.\n Fecha de la compra: '+ fecha.toLocaleString()+' \n El total de tu compra es:  '+ Number(totalCompras) +' \n La cantidad de productos comprados es de : '+ Number(listaCompras.length) +'\n | VER EN CONSOLA SUS COMPRAS Y CARACTERISTICAS |'); */
 
-/* ***************************************************************************************************************************************** *************************************************************DOM**************************************************************************
-****************************************************************************************************************************************** */
-let cantidadHTML = document.getElementById("cantidadHTML");
-    cantidadHTML.innerText = listaCompras.length;
-for (const datosArray of listaCompras) {
-    
-    let categoriaHTML = document.getElementById("categoriaHTML");
-    categoriaHTML.innerText = datosArray.tipo;
+/**************************************************************************************************************************************** *************************************************************DOM*************************************************************************
+*****************************************************************************************************************************************/
+// RECORDATORIO
+// -    CREAR UN EXCEL CON LOS ARTICULO PARA VENDER Y PODER PASARLO AL HTML
 
-    let colorHTML = document.getElementById("colorHTML");
-    colorHTML.innerText = datosArray.color;
+//  VARIABLES DOM
 
-    let talleHTML = document.getElementById("talleHTML");
-    talleHTML.innerText = datosArray.talle;
-     
+let menorPrecio = document.getElementById("menorPrecio");
+let mayorPrecio = document.getElementById("mayorPrecio");
+
+let precioFinal = [];
+
+// FILTROS NUMERICOS
+
+let codigoCompra = document.getElementById("codigoCompra");
+
+codigoCompra.addEventListener("change",vericicacionNumerica = () =>{
+    if(isNaN(codigoCompra.value)){
+        alert('Ingrese Números')
+    }
+});
+
+// ESCRIBIR HTML 
+let itemLista;
+let botonPedir = document.getElementById("pedir");
+function escribirhtml(){
+    let listaComprasNueva = document.getElementById("listaComprasNueva");
+    itemLista = document.createElement("li"); 
+    itemLista.innerHTML = ` <h5>Detalles de su compra<h5/>
+        <p>tipo: ${tipoRopa.value}</p>
+        <p>cantidad: ${cantidad}</p>
+        <p>codigo: ${codigo.value}</p>
+        <p>color: ${color}</p>
+        <p>talle: ${talle}</p>
+        <p>Precio: ${cTotal() * cantidad}</p> 
+        `
+    itemLista.className = "fs-4 nav-item d-flex flex-column text-light nuevoItem"
+    listaComprasNueva.append(itemLista);
+}
+
+botonPedir.addEventListener("click",escribirhtml);
+
+
+// LIMPIAR LISTA DE FILTRO
+
+function borrarFiltro(){
+    let nuevosItem = listaComprasNueva.querySelectorAll(".nuevoItem"); // eliminando todos los hijos creados de listaCompraNueva
+    for (const i of nuevosItem) { 
+        i.remove()
+    }
+    console.log(itemLista);
+    contador('limpiar');
+    codigo.value = 0;
+}
+
+let limpiar = document.getElementById("limpiar");
+limpiar.addEventListener("click",borrarFiltro);
+
+// OCULTAR FILTRO
+
+let d = 0;
+let botonFiltro = document.getElementById("botonFiltro");
+botonFiltro.addEventListener("click",ocultarFiltro = () =>{
+    if (d==0) {
+        filtro.className = "displayNone";
+        d++;
+    }else{
+        filtro.className = "displayActive d-flex flex-column bg-secondary py-3 border-dark border-bottom border-1";
+        d--;
+    }    
+})
+
+// PEDIR TIPO DE ROPA
+
+let tipoRopa = document.getElementById("tipoRopa");
+tipoRopa.addEventListener("change", MostrarTipoRopa = () =>{
+    //console.log(tipoRopa.value); 
+    cTotal(tipoRopa.value);
+})
+
+// PEDIR CANTIDAD 
+
+let cantidad = 0;
+let agregar = document.getElementById("agregar");
+let quitar = document.getElementById("quitar");
+let cantidadTotal = document.getElementById("cantidadTotal");
+
+function contador (x){
+
+    if (x === 'limpiar') {
+        cantidad = 0;
+        cantidadTotal.innerText = Number(cantidad);
+    }else{
+        cantidad+=x;
+        if (cantidad <= 0) {
+            cantidad = 0;
+        }
+        cantidadTotal.innerText = Number(cantidad);
+    }
+   
+    return cantidad 
+}
+agregar.addEventListener("click",agregar = ()=>{
+    contador(+1);
+    mostrarPrecioMomento(+1);
+})
+quitar.addEventListener("click",quitar = ()=>{
+    contador(-1);
+    mostrarPrecioMomento(-1);
+})
+
+// TOMAR CODIGO 
+
+let codigo = document.getElementById("codigoCompra");
+codigo.addEventListener("change",tomarCodigo = () =>{
+    console.log(codigo.value);;
+})
+
+// TOMAR COLORES
+
+let blanco = document.getElementById("colorBlanco");
+blanco.addEventListener("change",mostrar = () =>{
+    color = "blanco"
+})
+let negro = document.getElementById("colorNegro");
+negro.addEventListener("change",mostrar = () =>{
+    color = "negro"
+})
+let rojo = document.getElementById("colorRojo");
+rojo.addEventListener("change",mostrar = () =>{
+    color = "rojo"
+})
+let diseño = document.getElementById("colorDiseño");
+diseño.addEventListener("change",mostrar = () =>{
+    color = "diseño"
+})
+
+// PEDIR MEDIDAS
+let talle;
+let S = document.getElementById("S");
+S.addEventListener("change",mostrar = () =>{
+    talle = "s"
+})
+let M = document.getElementById("M");
+M.addEventListener("change",mostrar = () =>{
+    talle = "m"
+})
+let L = document.getElementById("L");
+L.addEventListener("change",mostrar = () =>{
+    talle = "l"
+})
+let XL = document.getElementById("XL");
+XL.addEventListener("change",mostrar = () =>{
+    talle = "xl"
+})
+let XXL = document.getElementById("XXL");
+XXL.addEventListener("change",mostrar = () =>{
+    talle = "xxl"
+})
+
+// CANTIDAD TOTAL
+let tipo;
+let f=0;
+let total=0;
+function cTotal (c){
+    switch (c) {
+        case 'pantalon':
+            tipo = 3400;
+        break;
+        case 'remera':
+            tipo = 2000; 
+            break;
+        case 'gorra':
+            tipo = 1000;
+        break;
+        case 'accesorio':
+            tipo = 600;
+            break;
+        case 'buzo':
+            tipo = 6000;
+        break;
+        default:
+            break;
+    }
+
+    return tipo
+}
+pedir.addEventListener("click", sumarCantidad = () =>{
+    precioFinal.push(cTotal() * cantidad);
+    console.log(precioFinal);
+})
+let b =0;
+function mostrarPrecioMomento (a) {
+
+    b += Number(a);
+    //console.log(Number(b));
+    if (b <= 0) {
+        b = 0;
+    }
+    let precioTotal = document.getElementById("precioTotal");
+    precioTotal.innerText = ( cTotal() * Number(b));
 }
