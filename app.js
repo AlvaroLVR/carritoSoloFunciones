3e5ee8b1f332f21a97fb02f7f61a5f38754d6eb1
alvaro valencia <alvaroleandro74@gmail.com>
2022-06-08 15:48:32 -0300

    eliminando codigo

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Carrito de Compras
-/* FEATURES: BOTON DE FAVORITO - TOTALIZAR LA COMPRA CON TODOS LOS ARTICULO AGREGADOS AL CARRITO - OPCIONES DE PRECIO USD/EURO/ -
-AGREGAR UN SUMADOR DE PRODUCTOS AL CARRITO DE COMPRAS 
+/* FEATURES: BOTON DE FAVORITO - TOTALIZAR LA COMPRA CON TODOS LOS ARTICULO AGREGADOS AL CARRITO - OPCIONES DE PRECIO USD/EURO/ - AGREGAR UN SUMADOR DE PRODUCTOS AL CARRITO DE COMPRAS 
  */
 let objetoComparador = {};
 
@@ -35,8 +34,6 @@ guardarLocal('inventario',JSON.stringify(inventario));
 
 
 escribirHTML(inventario)
-// BOTON AGREGAR/QUITAR A FAVORITO 
-
 
 // PEDIR TIPO DE ROPA
 
@@ -71,21 +68,13 @@ XL.addEventListener("change",mostrar = () =>{   objetoComparador.talle =    'XL'
 let XXL = document.getElementById("XXL");
 XXL.addEventListener("change",mostrar = () =>{  objetoComparador.talle =    'XXL'})
 
-// PEDIR CODIGO
-
-let codigo = document.getElementById("codigoCompra");
-codigo.addEventListener("change",tomarCodigo = () =>{
-    objetoComparador.codigo =  codigo.value;
-    vericicacionNumerica(codigo.value);
-})
-
 // FILTROS NUMERICOS
 
 function vericicacionNumerica(a){
     isNaN(codigoCompra.value) ? alert('Ingrese Números') : '';
 }
 
-// FUNCION RECIBIDORA DE DATOR E IMPRIMIR HMTML
+// FUNCION RECIBIDORA DE DATOS E IMPRIMIR HMTML
 
 let botonPedir = document.getElementById('buscar');
 botonPedir.addEventListener('click',tomarDatos = () =>{
@@ -117,11 +106,12 @@ function escribirHTML(a) {
     a.forEach(i => {
         let catalogoNew1 = document.createElement('div');
         catalogoNew1.innerHTML= `
-        <div class="d-flex shadow flex-row mb-3 p-2 align-items-center justify-content-between" style="width: auto; height: auto;position: relative">
+        <div class="d-flex shadow flex-row mb-3 p-2 align-items-center justify-content-between bg-light rounded-1" style="width: auto; height: auto;position: relative">
             <svg class="fav" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"> <path fill="#e4e6e8" d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg>
-            <div class="d-flex flex-row">
-                <img src="./img/${i.producto}.jpg" class="border border-1 border-ligth rounded-1 imagen" alt="tienda1" height="200px" width="250">
-                <div class="ms-5">
+
+            <div class="d-flex flex-row" >
+                <img src="./img/${i.producto}.jpg" class="border border-1 border-ligth imagen" alt="tienda1" height="200px" width="250">
+                <div class="ms-5" id="datosCard">
                     <h5 class="">${i.producto}</h5>
                     <p class="">Precio:$ ${i.precio}</p>
                     <p class="">Talle: ${i.talle}</p>
@@ -129,9 +119,10 @@ function escribirHTML(a) {
                     <p class="">Codigo: ${i.codigo}</p>   
                 </div>
             </div>
-            <div class="d-flex flex-column justify-content-end me-4">
+
+            <div class="d-flex flex-column justify-content-end me-4" id="botones">
                 <button class="btn btn-secondary mb-3"><a href="#" class="text-light" style="text-decoration: none;">Comprar</a></button>
-                <button class="btn btn-secondary "><a href="#" class="text-light" style="text-decoration: none;">Añadir al carrito</a></button>
+                <button class="btn btn-secondary"><a href="#" id="agregarCarrito" class="text-light" style="text-decoration: none;">Añadir al carrito</a></button>
             </div>
         </div>` 
         catalogo.appendChild(catalogoNew1); 
@@ -160,3 +151,11 @@ cross.addEventListener('click',hola= ()=>{
     console.log('presionaste la cruz');
     carritoVentana.className = 'carritoVentanaNone border shadow rounded-2 bg-light d-flex flex-column';
 })
+
+
+// AGREGAR AL CARRITO
+
+let datosCard = document.getElementById('datosCard');
+let botonAgregar = document.getElementById('agregarCarrito');
+
+
